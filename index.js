8f7ea676d429527ea41621d2d441059de4016182
Ragini Kaushal <ragini93.11@gmail.com>
2023-07-25 09:09:18 +0530

    changes

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const app = express();
-// const { connection } = require("./config/db");
+
 const { trainRouter } = require("./routes/trains.routes");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-// const {validation} = require("./middleware/validation.middleware");
+
 
 // Middleware
 app.use(bodyParser.json());
-// app.use(validation);
+
 app.use(cors());
 app.use("/train", trainRouter);
-//process.env.PORT
+
 app.listen(8080, () => {
   console.log(`Server is connected to 8080`);
 });
--- routes/trains.routes.js
+++ routes/trains.routes.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const trainRouter = express.Router();
+// step-1
+// used 2d array dataStructure to make this seat
+// initialize each row inside element with zero which means seat is empty
+// for the last row take only three element to make seat count 80
 
 let seats = [
   [0, 0, 0, 0, 0, 0, 0], //0
@@ -15,32 +19,43 @@ let seats = [
   [0, 0, 0, 0, 0, 0, 0], //10
   [0, 0, 0], //11
 ];
-let emptySeats = [7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 3];
+// step-2 create one more array which says number of empty seat in each row
+let emptySeats = [7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 3];
+
+// step-3 create one another array which says the status of each row no of booked seat initialize with zero which means no seat is booked
+
 let status = [0, 0, 0, 0, 0, 0, 0, 0];
+// step-4 apply basic condition which one function
 
 const seatBookingPossible = (number) => {
+  // if seat number is greater than 7 which means you can not book the seats
   if (number > 7) {
     return false;
+    // other wise return true
   } else {
     return true;
   }
 };
+// step-5 make another function for handling all possible cases
 
 const seatBooking = (number) => {
   if (seatBookingPossible(number)) {
-    let flag = false;
+    // if number is less than 7 then run a loop on 2d array
     for (let i = 0; i < seats.length; i++) {
-      console.log("aaaa");
       console.log(emptySeats[i], number);
+      // if status[i]===1 which means you seat is already booked and return false
       if (status[i]) {
+        // check one more condition like i===11 means no more row is availabe to check
         if (i === 11) {
           return false;
         }
         continue;
       }
-
+      // if emptyseats array element is greater than number which means seat is available
       if (emptySeats[i] >= number) {
+
         let j;
+
         let startIndex = i === 11 ? 3 - emptySeats[i] : 7 - emptySeats[i];
         console.log("startIndex => currentRow ", startIndex, i);
         for (j = startIndex; j < startIndex + number; j++) {
